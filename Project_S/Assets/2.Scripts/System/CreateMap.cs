9f2cfa450c4bb9a90d80205ac1dc7982fa9e796b
foxrain <88512219+foxrain-05@users.noreply.github.com>
2023-08-06 22:52:13 +0900

    맵 랜덤 생성 구현 시도중인 코드입니다.

---
+++ Project_S/Assets/2.Scripts/System/CreateMap.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour
+{
+    [SerializeField] private GameObject _tilePrefab;
+
+    [SerializeField] private int Height = 10;
+    [SerializeField] private int Width = 10;
+
+    [Header("Perlin Noise")]
+    [SerializeField] private float scale = 1f;
+    [SerializeField] private float threshold = 0.5f;
+
+
+    void Start()
+    {
+        // 타일 생성
+        float harpSqrt3 = Mathf.Sqrt(3) * 0.5f;
+        float checkOddLayer;
+
+        for(int i = 0; i < Height; i++)
+        {
+            checkOddLayer = (i % 2) == 0 ? 0 : 0.5f;
+            for(int j = 0; j < Width; j++)
+            {
+                GameObject tile = Instantiate(_tilePrefab, new Vector3(j - checkOddLayer, 0, i * harpSqrt3), Quaternion.identity);
+                tile.transform.parent = this.transform;
+
+                randomTile(tile);
+            }
+        }
+
+    }
+
+    void randomTile(GameObject tile)
+    {
+        float x = tile.transform.position.x / scale;
+        float z = tile.transform.position.z / scale;
+
+        float noise = Mathf.PerlinNoise(x, z);
+
+        if(noise > threshold)
+        {
+            Destroy(tile);
+        }
+    }
+}
